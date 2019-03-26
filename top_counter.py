fc106f73a68ec1a0be5e45f3f93a351a5d9fdb0d
Dario Mapelli <mapelli.dario@gmail.com>
2019-03-26 18:05:10 +0100

    implemented use of multiprocess in top_counter

--- top_counter.py
+++ top_counter.py
@@ -3,22 +3,18 @@ import sys
 import utils
 import os
 import argparse
+import multiprocessing
+from functools import partial
+from contextlib import contextmanager
 
-parser = argparse.ArgumentParser(description='Count how many t and tbar are present in every event')
-parser.add_argument('data_path', type=str, help='All the files in this directory will be analyzed')
-parser.add_argument('--nevents', type=int, default=-1, help='How many events per file should be analysed. Set to a negative value to include all events')
-parser.add_argument('--debug', type=bool, default=False, help='Set debug mode')
 
-args = parser.parse_args()
+@contextmanager
+def poolcontext(*args, **kwargs):
+    pool = multiprocessing.Pool(*args, **kwargs)
+    yield pool
+    pool.terminate()
 
-data_path = args.data_path
-nevents = args.nevents
-debug = args.debug
-file_list = os.listdir(data_path)
-
-print 'file tot_events no_t_no_tbar t tbar t_tbar'
-
-for f in sorted(file_list):
+def count_t_tbar(f, data_path, nevents, debug):
     file = rt.TFile(data_path + '/' + f)
     tree = file.Get("latino")
 
@@ -54,14 +50,24 @@ for f in sorted(file_list):
             count_no_t_tbar += 1
         # print index_t, index_tbar
 
-        mass_pairs = utils.mjj_pairs(partons)
-        eta_pairs = utils.deltaeta_pairs(partons)
-        # print "Mass pairs"
-        # print mass_pairs
-        # print "Eta pairs"
-        # print eta_pairs
-
         iev+=1
         if nevents > 0 and iev >= nevents:
             break
-    print f, iev, count_no_t_tbar, count_t, count_tbar, count_t + count_tbar + count_no_t_tbar - iev
+    result = [f, iev, count_no_t_tbar, count_t, count_tbar, count_t + count_tbar + count_no_t_tbar - iev]
+    print result
+    return result
+
+if __name__ == '__main__':
+
+    parser = argparse.ArgumentParser(description='Count how many t and tbar are present in every event')
+    parser.add_argument('data_path', type=str, help='All the files in this directory will be analyzed')
+    parser.add_argument('--nevents', type=int, default=-1, help='How many events per file should be analysed. Set to a negative value to include all events')
+    parser.add_argument('--debug', type=bool, default=False, help='Set debug mode')
+    parser.add_argument('--processes', type=int, default=8, help='Number of concurrent processes')
+    args = parser.parse_args()
+
+    file_list = sorted(os.listdir(args.data_path))
+    print 'file tot_events no_t_no_tbar t tbar t_tbar'
+    with poolcontext(processes=args.processes) as pool:
+        results = pool.map(partial(count_t_tbar, data_path=args.data_path, nevents=args.nevents, debug=args.debug), file_list)
+    print sorted(results)
