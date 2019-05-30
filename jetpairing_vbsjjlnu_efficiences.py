cdddc7ec876b10e7f368d2d5d44aa3cc81a7d23c
Dario Mapelli <mapelli.dario@gmail.com>
2019-05-30 12:01:43 +0200

    jetpairing efficiencies vbsjjlnu: produces nice summary tables

--- jetpairing_vbsjjlnu_efficiences.py
+++ jetpairing_vbsjjlnu_efficiences.py
@@ -8,6 +8,7 @@ from functools import partial
 from contextlib import contextmanager
 import pprint
 import json
+import pandas as pd
 
 @contextmanager
 def poolcontext(*args, **kwargs):
@@ -16,17 +17,19 @@ def poolcontext(*args, **kwargs):
     pool.terminate()
 
 def jetpairing_ecciciency(file_list, data_path, nevents):
+    pairing_algorithm = 'vjets' if data_path.rfind('VjetpairAndVars') != -1 else 'vbsjets'
+    file_mask = file_list[0][:file_list[0].find('__')]
     pairing_result = {
+        'tag': file_mask,
         'file_list': file_list,
         'events': 0,
         'events_notop': 0,
         'events_notop_matched': 0,
-        'events_notop_matched_vjets': 0,
-        'events_notop_matched_vjets_vbsjets': 0
+        'events_notop_matched_first': 0,
+        'events_notop_matched_second': 0
     }
-    file_mask = file_list[0][:file_list[0].find('__')]
-    vjets_eff   = rt.TEfficiency ('vjets'+file_mask, 'vjets efficiency', 15, 0, 400)
-    vbsjets_eff = rt.TEfficiency ('vbsjets'+file_mask, 'vbsjets efficiency', 15, 0, 1200)
+    first_eff  = rt.TEfficiency ('first'+file_mask, 'Correct first match vjets;mjj vbsjets [GeV];Efficiency', 15, 0, 1200)
+    second_eff = rt.TEfficiency ('second'+file_mask, 'Correct both matches;mjj vbsjets [GeV];Efficiency', 15, 0, 1200)
     for current_file in file_list:
         infile = rt.TFile(data_path + '/' + current_file)
         tree = infile.Get("latino")
@@ -37,29 +40,51 @@ def jetpairing_ecciciency(file_list, data_path, nevents):
             pairing_result['events'] += 1
             if event.hasTopGen == 0.:
                 pairing_result['events_notop'] += 1
-            if event.PartonJetMatchFlag == 0:
-                pairing_result['events_notop_matched'] += 1
-                vbsjets_eff.Fill(vjetsmatch and vbsjetsmatch, event.mjj_vbs)
-                vjets_eff.Fill(vjetsmatch, event.mjj_vjet)
-            if vjetsmatch:
-                pairing_result['events_notop_matched_vjets'] += 1
-            if vjetsmatch and vbsjetsmatch:
-                pairing_result['events_notop_matched_vjets_vbsjets'] += 1
+                if event.PartonJetMatchFlag == 0:
+                    pairing_result['events_notop_matched'] += 1
+                    second_eff.Fill(vjetsmatch and vbsjetsmatch, event.mjj_vbs)
+                    if pairing_algorithm == 'vjets':
+                        first_eff.Fill(vjetsmatch, event.mjj_vbs)
+                    else:
+                        first_eff.Fill(vbsjetsmatch, event.mjj_vbs)
+                # IL SEGUENTE BLOCCO DEVE STARE QUI, NON SPOSTARE FUORI DA QUESTO IF
+                    if pairing_algorithm == 'vjets':
+                        if vjetsmatch:
+                            pairing_result['events_notop_matched_first'] += 1
+                    else:
+                        if vbsjetsmatch:
+                            pairing_result['events_notop_matched_first'] += 1
+                    if vjetsmatch and vbsjetsmatch:
+                        pairing_result['events_notop_matched_second'] += 1
             iev+=1
             if nevents > 0 and iev>= nevents:
                 break
-    pairing_result['efficiency_notop_matched_vjets']         = float(pairing_result['events_notop_matched_vjets'])         / float (pairing_result['events_notop_matched'] )
-    pairing_result['efficiency_notop_matched_vjets_vbsjets'] = float(pairing_result['events_notop_matched_vjets_vbsjets']) / float (pairing_result['events_notop_matched'] )
+    pairing_result['efficiency_notop_matched_first']  = float(pairing_result['events_notop_matched_first'])  / float (pairing_result['events_notop_matched'] )
+    pairing_result['efficiency_notop_matched_second'] = float(pairing_result['events_notop_matched_second']) / float (pairing_result['events_notop_matched'] )
     outfile = rt.TFile('../' + file_mask + '.root', 'RECREATE')
-    vjets_eff.Write()
-    vbsjets_eff.Write()
+    first_eff.Write()
+    second_eff.Write()
     outfile.Close()
     return pairing_result
+
+def render_as_table(df):
+    df['tag'] = df['tag'].apply(lambda x: x[7:])
+    df['tag'] = df['tag'].apply(lambda x: x.split('_'))
+    df['tag'] = df['tag'].apply(lambda x: str(x[0]) + str(' ' *(8-len(x[0]))) + str(x[1]))
+    df = df.rename(columns={'efficiency_notop_matched_first': 'first'})
+    df = df.rename(columns={'efficiency_notop_matched_second': 'second'})
+    df['first'] = df['first'].round(2)
+    df['second'] = df['second'].round(2)
+    df = df.set_index('tag')
+    df = df.sort_values('second', ascending=False)
+    print df[['first', 'second']]
 
 if __name__ == '__main__':
     '''
     VBSjjlnu:
-    python jetpairing_vbsjjlnu_efficiences.py /gwteray/users/govoni/OneLeptonSkims/VBS_semileptonic_signal_summer16/lepSel__MCWeights__bSFLpTEffMulti__cleanTauMC__l1tightChain__bvetoTight__LepTrgFix__dorochester__formulasMC__resolvedVBSPairingGenAndVars --nevents -1
+    python jetpairing_vbsjjlnu_efficiences.py /gwteray/users/govoni/OneLeptonSkims/VBS_semileptonic_signal_summer16/lepSel__MCWeights__bSFLpTEffMulti__cleanTauMC__l1tightChain__bvetoTight__LepTrgFix__dorochester__formulasMC__formulasMC__gr4JetsSkim__JetPairingGenVBS__VjetpairAndVars --nevents -1
+
+    ACHTUNG! This script expects the full pathname to contain only once the string "VjetpairAndVars" or "VBSjetpairAndVars"
     '''
     parser = argparse.ArgumentParser(description='Compute jetpairing efficiencies')
     parser.add_argument('data_path', type=str, help='All the files in this directory will be analyzed')
@@ -69,11 +94,11 @@ if __name__ == '__main__':
     args = parser.parse_args()
 
     file_list = sorted(os.listdir(args.data_path))
-    file_maks_set = set()
-    for file in file_list:
-        file_maks_set.add(file[:file.find('__')])
+    file_mask_set = set()
+    for f in file_list:
+        file_mask_set.add(f[:f.find('__')])
     file_list_grouped = []
-    for file_mask in file_maks_set:
+    for file_mask in file_mask_set:
         file_mask_list = []
         for file in file_list:
             if file.find(file_mask) != -1:
@@ -84,7 +109,33 @@ if __name__ == '__main__':
     # file_list = ['latino_WmTo2J_ZTo2L__part0.root']
     with poolcontext(processes=args.processes) as pool:
         results = pool.map(partial(jetpairing_ecciciency, data_path=args.data_path, nevents=args.nevents), file_list_grouped)
-    pprint.pprint (results)
-    with open('../pairing_result.json', 'w') as result_file:
-        json.dump(results, result_file)
+    render_as_table(pd.DataFrame(results))
+    
+    # aggregate cumulative results
+    results_aggregated = {
+        'events': 0,
+        'events_notop': 0,
+        'events_notop_matched': 0,
+        'events_notop_matched_first': 0,
+        'events_notop_matched_second': 0,
+        'efficiency_notop_matched_first': 0.0,
+        'efficiency_notop_matched_second': 0.0
+    }
+    for result in results:
+        results_aggregated['events'] += result['events']
+        results_aggregated['events_notop'] += result['events_notop']
+        results_aggregated['events_notop_matched'] += result['events_notop_matched']
+        results_aggregated['events_notop_matched_first'] += result['events_notop_matched_first']
+        results_aggregated['events_notop_matched_second'] += result['events_notop_matched_second']
+
+    results_aggregated['efficiency_notop_matched_first']  = float(results_aggregated['events_notop_matched_first'])  / float (results_aggregated['events_notop_matched'] )
+    results_aggregated['efficiency_notop_matched_second'] = float(results_aggregated['events_notop_matched_second']) / float (results_aggregated['events_notop_matched'] )
+    pprint.pprint(results_aggregated)
+
+    with open('../pairing_' + str( args.data_path[args.data_path.rfind('__')+2:] ) + '.json', 'w') as result_file:
+        result_combination = {
+            'aggregated': results_aggregated,
+            'single_process': results
+        }
+        json.dump(result_combination, result_file)
 
