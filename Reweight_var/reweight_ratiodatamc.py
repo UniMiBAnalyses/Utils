c160d4732a2c07a23473952b3639aef6bb17cedf
Davide Valsecchi <davide.valsecchi@cern.ch>
2020-04-15 16:44:04 +0200

    Added simple graph output for var reweighting

--- Reweight_var/reweight_ratiodatamc.py
+++ Reweight_var/reweight_ratiodatamc.py
@@ -15,6 +15,7 @@ parser.add_argument("--file", type=str, help="mkShape rootFile")
 parser.add_argument("--output", type=str, help="Path to file containing the ratios")
 parser.add_argument("--var",type=str, help="Variable whose disrtibution is used to compute the new set of weights")
 parser.add_argument("--samples",nargs="+", type=str, help="Samples (space-separated list)")
+parser.add_argument("--sample-to-reweight",type=str, help="Sample to be reweighted")
 parser.add_argument("--cat", type=str, help="Category (only a single category is considered!)")
 args = parser.parse_args()
 
@@ -27,7 +28,6 @@ f = R.TFile(file)
 hs = {}
 tot_mc = None
 
-
 for s in samples:
     h = f.Get(cat+ "/"+args.var+"/histo_"+s)
     print (h)
@@ -39,24 +39,19 @@ for s in samples:
         tot_mc.SetName("totMC") 
 
 data_hist = f.Get(cat+ "/"+args.var+"/histo_DATA")
+# Remove all others MC from data
+data_hist.Add(tot_mc, -1)
 
-# No fake in DY under Z peak
-# if 'mm' in args.cat:
-#     fake_sample = '/histo_Fake_mm'
-# elif 'ee' in args.cat:
-#     fake_sample = '/histo_Fake_ee'
-# else:
-#     print 'no fake sample selected, ERROR'
-# fake_hist =  f.Get(cat+ "/" + args.var + fake_sample)
-# data_hist.Add(fake_hist, -1)
+reweight_hist = f.Get(cat + "/"+ args.var +"/histo_"+args.sample_to_reweight)
+
 
 # Ratio: normalized distribution!
-tot_mc.Scale(1/ tot_mc.Integral())
+reweight_hist.Scale(1/ reweight_hist.Integral())
 data_hist.Scale(1/data_hist.Integral())
 
 c = R.TCanvas()
-tot_mc.Draw("hist")
-tot_mc.SetLineColor(R.kRed)
+reweight_hist.Draw("hist")
+reweight_hist.SetLineColor(R.kRed)
 data_hist.Draw("hist same")
 c.Draw()
 
@@ -65,23 +60,36 @@ nbins = data_hist.GetNbinsX()
 weights = []
 x = []
 errw = []
+gr = R.TGraphErrors()
+
 
 for ibin in range(1, nbins+1):
-    x.append(tot_mc.GetXaxis().GetBinLowEdge(ibin))
-    if tot_mc.GetBinContent(ibin) == 0: 
+    x.append(reweight_hist.GetXaxis().GetBinLowEdge(ibin))
+    if reweight_hist.GetBinContent(ibin) == 0: 
         # used for ibin == 1 case
         weights.append(1.)
         continue
-    weights.append(data_hist.GetBinContent(ibin) / tot_mc.GetBinContent(ibin))
-    errw.append( sqrt(  (1/tot_mc.GetBinContent(ibin))**2 * data_hist.GetBinError(ibin)**2  \
-                + ( data_hist.GetBinContent(ibin)/ tot_mc.GetBinContent(ibin)**2 )**2 * tot_mc.GetBinError(ibin)**2))
+    
+    w = data_hist.GetBinContent(ibin) / reweight_hist.GetBinContent(ibin)
+    weights.append(w)
+    gr.SetPoint(ibin, data_hist.GetBinCenter(ibin), w)
+
+    e = sqrt(  (1/reweight_hist.GetBinContent(ibin))**2 * data_hist.GetBinError(ibin)**2  \
+                + ( data_hist.GetBinContent(ibin)/ reweight_hist.GetBinContent(ibin)**2 )**2 * reweight_hist.GetBinError(ibin)**2)
+    errw.append( e)
+    gr.SetPointError(ibin, 0., e)
+
 
 
 #wsum = sum(weights)
 #norm_weights = [w / wsum for w in weights]
 
-with open(args.output , "w") as out:
-    for x,w,err in zip(x,weights, errw):
-        out.write("{:.0f} {} 0. {}\n".format(x,w, err)) 
+# with open(args.output , "w") as out:
+#     for x,w,err in zip(x,weights, errw):
+#         out.write("{:.0f} {} 0. {}\n".format(x,w, err)) 
 
-
+outfile = R.TFile(args.output , "RECREATE")
+gr.SetName("weights")
+gr.Write()
+outfile.Write()
+outfile.Close()
